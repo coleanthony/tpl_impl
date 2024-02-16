5c8f9915b2fa8031b28c29a2a6ea2c78cb0eba33
coleanthony <971259967@qq.com>
2024-02-16 11:21:30 +0800

    finish skiplist

--- skiplist/skiplist_impl.h
+++ skiplist/skiplist_impl.h
@@ -2,7 +2,8 @@
 //created by cwh in 2024/2/15
 #pragma once
 
-#include "util/random.h"
+#include <cassert>
+#include "../util/random.h"
 
 template <typename Key, class Comparator>
 class SkipList {
@@ -13,6 +14,8 @@ private:
     Comparator const compare_;
     Node* const head_;
     Random rnd_;
+
+    int keynum; //for test
 
     inline int GetMaxHeight() const{
         return max_height_;
@@ -39,6 +42,7 @@ private:
 
 public:
     explicit SkipList(Comparator cmp);
+	~SkipList();
     SkipList(const SkipList&) = delete;
     SkipList& operator=(const SkipList&) = delete;
     // Insert key into the list.
@@ -48,6 +52,12 @@ public:
     bool Contains(const Key& key) const;
     // delete
     bool Delete(const Key& key);
+    //clear all the data in the skiplist
+    void Clear();
+    //for test
+    int GetKeyNum(){
+        return keynum;
+    } 
 };
 
 //
@@ -59,7 +69,7 @@ struct SkipList<Key, Comparator>::Node {
     // Accessors/mutators for links.  Wrapped in methods so we can
     // add the appropriate barriers as necessary.
     Node* Next(int n) {
-        assert(n >= 0);
+        assert((n >= 0));
         return next_[n];
     }
     void SetNext(int n, Node* x) {
@@ -74,7 +84,7 @@ private:
 
 //注意template和typename的使用，返回一个SkipList<Key, Comparator>::Node*类型变量
 template <typename Key, class Comparator>
-SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::NewNode(const Key& key,int height){
+typename SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::NewNode(const Key& key,int height){
     char* node_memory=new char[sizeof(Node)+sizeof(Node*)*height];
     return new (node_memory) Node(key);
 }
@@ -85,7 +95,7 @@ int SkipList<Key,Comparator>::RandomHeight(){
     static const int RandomK=4;
     int height=1;
     while (height<KMaxHeight){
-        if (!OneIn(RandomK)){
+        if (!rnd_.OneIn(RandomK)){
             break;
         }
         height++;
@@ -104,7 +114,7 @@ bool SkipList<Key,Comparator>::KeyIsBeforeNode(const Key& key, Node* n) const{
 }
 
 template <typename Key, class Comparator>
-SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindGreaterOrEqual(const Key& key, Node** prev) const{
+typename SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindGreaterOrEqual(const Key& key, Node** prev) const{
     Node* p=head_;
     int height=GetMaxHeight()-1;
     while (true){
@@ -130,41 +140,122 @@ SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindGreaterOrEqual(con
 }
 
 template <typename Key, class Comparator>
-SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindLessThan(const Key& key) const{
-    Node* p=head;
+typename SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindLessThan(const Key& key) const{
+    Node* p=head_;
     int height=GetMaxHeight()-1;
     while (true){
         Node* next=p->Next(height);
-        if (KeyIsBeforeNode(key,next)){
-            
+        if (next!=nullptr&&compare_(key,next->key)>=0){
+            //next.val>=key
+            if (height>0){
+                height--;
+            }else{
+                return p;
+            }
         }else{
-            
+            p=next;
         }
     }
-    
+    return nullptr;
 }
 
 template <typename Key, class Comparator>
-SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindLast() const{
-
+typename SkipList<Key,Comparator>::Node* SkipList<Key,Comparator>::FindLast() const{
+    Node *p=head_;
+    int height=GetMaxHeight()-1;
+    while (true){
+        Node* next=p->Next(height);
+        if (next==nullptr){
+            if (height>0){
+                height--;
+            }else{
+                return p;
+            }
+        }else{
+            p=next;
+        }
+    }
+    return nullptr;
 }
 
 template <typename Key, class Comparator>
-SkipList<Key,Comparator>::SkipList(Comparator cmp){
+SkipList<Key,Comparator>::SkipList(Comparator cmp):
+    compare_(cmp),
+    head_(NewNode(0,KMaxHeight)),
+    rnd_(0xdeadbeef),
+    max_height_(1),
+    keynum(0){
+    for(int i=0;i<KMaxHeight;i++){
+        head_->SetNext(i,nullptr);
+    }
+}
 
+template <typename Key, class Comparator>
+SkipList<Key,Comparator>::~SkipList(){
+    Node* p=head_;
+    while (p){
+        Node* cur=p;
+        p=p->Next(0);
+        delete cur;
+    }
+}
+
+template <typename Key, class Comparator>
+void SkipList<Key,Comparator>::Clear(){
+    Node* p=head_->Next(0);
+    while (p){
+        Node* cur=p;
+        p=p->Next(0);
+        delete cur;
+    }
+    max_height_=1;
+        for(int i=0;i<KMaxHeight;i++){
+        head_->SetNext(i,nullptr);
+    }
+    keynum=0;
 }
 
 template <typename Key, class Comparator>
 void SkipList<Key,Comparator>::Insert(const Key& key){
+    Node* prev[KMaxHeight];
+    int height=RandomHeight();
+    Node* p=FindGreaterOrEqual(key,prev);
+    assert(p==nullptr||!Equal(p->key,key));
 
+    if (height>max_height_){
+        for (int i = 0; i < height; i++){
+            prev[i]=head_;
+        }
+        max_height_=height;
+    }   
+    
+    Node* newnode=NewNode(key,height);
+    for (int i = 0; i < height; i++){
+        newnode->SetNext(i,prev[i]->Next(i));
+        prev[i]->SetNext(i,newnode);
+    }
+    keynum++;
 }
 
 template <typename Key, class Comparator>
 bool SkipList<Key,Comparator>::Contains(const Key& key) const{
-
+    Node *p=FindGreaterOrEqual(key,nullptr);
+    if (p!=nullptr&&Equal(key,p->key)){
+        return true;
+    }
+    return false;
 }
 
 template <typename Key, class Comparator>
 bool SkipList<Key,Comparator>::Delete(const Key& key){
-
+    Node* prev[KMaxHeight];
+    Node* p=FindGreaterOrEqual(key,prev);
+    assert(p!=nullptr&&Equal(p->key,key));
+    for (int i = 0; i < GetMaxHeight(); i++){
+        if (p[i]==nullptr){
+            break;
+        }
+        prev[i]->SetNext(i,p[i]->Next(i));
+    }
+    keynum--;
 }
--- skiplist/skiplist_test.cpp
+++ skiplist/skiplist_test.cpp
@@ -1,3 +1,59 @@
 //skiplist_test.cpp
 //created by cwh in 2024/2/15
 
+#include "skiplist_impl.h"
+#include "../util/random.h"
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+typedef uint64_t Key;
+
+struct Comparator {
+  int operator()(const Key& a, const Key& b) const {
+    if (a < b) {
+      return -1;
+    } else if (a > b) {
+      return +1;
+    } else {
+      return 0;
+    }
+  }
+};
+
+int main(){
+    const int N = 2000;
+    const int R = 5000;
+    Random rnd(1000);
+    std::set<Key> keys;
+    Comparator cmp;
+    SkipList<Key, Comparator> list(cmp);
+    std::cout<<"listnum:"<<list.GetKeyNum()<<std::endl;
+    for (int i = 0; i < N; i++) {
+        Key key = rnd.Next() % R;
+        if (keys.insert(key).second) {
+            list.Insert(key);
+        }
+    }
+    std::cout<<"after insertion, listnum:"<<list.GetKeyNum()<<std::endl;
+
+    for (int i = 0; i < R; i++) {
+        if (list.Contains(i)&&!keys.count(i)) {
+            std::cout<<"insert data error"<<std::endl;
+        }
+    }
+
+    list.Clear();
+    std::cout<<"after clead, listnum:"<<list.GetKeyNum()<<std::endl;
+
+    list.Insert(0);
+    list.Insert(1);
+    list.Insert(3);
+    list.Insert(10);
+    list.Insert(7);
+    list.Insert(9);
+    std::cout<<"after insert, listnum:"<<list.GetKeyNum()<<std::endl;
+
+    std::cout<<"test successfully"<<std::endl;
+}
